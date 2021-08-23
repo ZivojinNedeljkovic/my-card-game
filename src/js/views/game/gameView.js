2e0f312466781c8be9cd162572490e2ac4938102
ZivojinNedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2021-08-23 18:33:50 +0200

    animation problems fixed

--- src/js/views/game/gameView.js
+++ src/js/views/game/gameView.js
@@ -35,7 +35,7 @@ class GameView extends View {
     this.playersDeck.render(playersCards, maxCardsOfType)
     this.swipeAria.render()
 
-    this.#dealCards()
+    this.#dealCards(false)
     this.playersDeck.onClickHandler(this.#putCardOnTable.bind(this))
   }
 
