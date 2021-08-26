c90db721251c4d422c1c91e610aec13ed886a1c9
ZivojinNedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2021-08-26 02:00:38 +0200

    experiment

--- src/js/controllers/inputController.js
+++ src/js/controllers/inputController.js
@@ -1,6 +1,7 @@
 import { examineCombination } from '../models/combinationModel'
 import { state } from '../models/state'
 import GameView from '../views/game/gameView'
+import * as config from '../models/config'
 
 export function submitCombController() {
   if (!GameView.currentMove) return
@@ -8,8 +9,10 @@ export function submitCombController() {
     GameView.currentMove
   )
   GameView.renderMoveResult(rightCardsInRightPlaces, rightCards) /
-  GameView.startNewMove()
+    GameView.startNewMove()
   state.movesPlayed++
+  if (state.movesPlayed >= config.NUMBER_OF_TRAYS)
+    alert(state.winningCombination)
 }
 
 export function returnCardController() {
